4d1d690fa5892a878328d22610b065a302857596
Samuel Antonio Gonzales Garcia <sgonzgar@everis.com>
2021-07-06 12:57:55 -0500

    add exception handler

--- src/main/java/cl/bci/test/samus/bcitest/rest/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/cl/bci/test/samus/bcitest/rest/exception/RestResponseEntityExceptionHandler.java
@@ -4,6 +4,7 @@ import cl.bci.test.samus.bcitest.exception.EmailExistsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,6 +23,21 @@ public class RestResponseEntityExceptionHandler {
     @ExceptionHandler(WebExchangeBindException.class)
     public Map<String, String> handleValidationExceptions(
             WebExchangeBindException ex) {
+        log.info("MethodArgumentNotValidException");
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
         log.info("MethodArgumentNotValidException");
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
